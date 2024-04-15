7ec0ba0a1b00057156f98dc39be5db9b3017e952
mrvzay <vzay1999@gmail.com>
2024-04-15 15:09:59 +0530

    add new lectures

---
+++ ImmutableClasses/GameConsole/src/dev/lpa/sealed/SpecialAbstractClass.java
@@ -0,0 +1,9 @@
+package dev.lpa.sealed;
+
+public sealed abstract class SpecialAbstractClass permits FinalKid, NonSealedKid, SealedKid, SpecialAbstractClass.Kid {
+
+    final class Kid extends SpecialAbstractClass {
+
+    }
+
+}
